915dcf11d464657c59bc949683ec20f772fd34c4
Surya8628 <schint10@uncc.edu>
2025-07-20 11:27:15 -0600

    Mutliplication operation

---
+++ Multiplication/Multiplication.java
@@ -0,0 +1,19 @@
+package Multiplication;
+
+import java.util.Scanner;
+
+public class Multiplication {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter the first number: ");
+        double num1 = input.nextDouble();
+
+        System.out.println("Enter the second number: ");
+        double num2 = input.nextDouble();
+
+        double result = num1 * num2;
+
+        System.out.println(result);
+    }
+}
